35136f76d9c66d8baf3c84bf4ac678dc592eaa97
Isabella <bdc190799@gmail.com>
2023-09-12 22:37:14 +0100

    Current day display
    Updated the current day to display at the top of the page. Added functionality to append "st"/"nd"/"th" to the correct dates.

--- scripts/JavaScript/index.js
+++ scripts/JavaScript/index.js
@@ -9,8 +9,95 @@ let currentMonth = dayjs().month(); //used to display current month at the top o
 
 let calendarList = $("#calendarList"); //storing each block of the calendar in an array as a separate block element with consistent styling.
 let eventInputEl = $("#eventInput"); //getting each event input and storing the strings inputted
+let todayEl = $("#currentDay"); //will display the current day as the website was loaded.
 
-createCalendar();
+function displayToday()
+{
+    let dayText = "Default Day";
+    switch(currentDay) //translate each day from a number to readable string
+    {
+        case 0: //Sunday
+            dayText = "Sunday";
+            break;
+        case 1:
+            dayText = "Monday";
+            break;
+        case 2:
+            dayText = "Tuesday";
+            break;
+        case 3:
+            dayText = "Wednesday";
+            break;
+        case 4:
+            dayText = "Thursday";
+            break;
+        case 5:
+            dayText = "Friday";
+            break;
+        case 6: //Saturday
+            dayText = "Saturday";
+            break;
+        default:
+            console.log(`Reached default day with value: ${currentDay}`); //debug in case of error
+            break;
+    }
+    let monthText = "Default Month";
+    switch(currentMonth) //translate each month from a number to readable string
+    {
+        case 0: //January
+            monthText = "January";
+            break;
+        case 1:
+            monthText = "February";
+            break;
+        case 2:
+            monthText = "March";
+            break;
+        case 3:
+            monthText = "April";
+            break;
+        case 4:
+            monthText = "May";
+            break;
+        case 5:
+            monthText = "June";
+            break;
+        case 6: 
+            monthText = "July";
+            break;
+        case 7: 
+            monthText = "August";
+            break;
+        case 8: 
+            monthText = "September";
+            break;
+        case 9: 
+            monthText = "October";
+            break;
+        case 10: 
+            monthText = "November";
+            break;
+        case 11: //December
+            monthText = "December";
+            break;
+        default:
+            console.log(`Reached default month with value: ${currentMonth}`); //debug in case of error
+            break;
+    }
+    if(currentDate === 1 || currentDate === 21 || currentDate === 31) //appending "st" to the 1st/21st/31st of a month
+    {
+        todayEl.text(`Today is: ${dayText}, ${currentDate}st of ${monthText}.`);
+    }
+    else if(currentDate === 2 || currentDate === 22) //appending "nd" to the 2nd/22nd of a month
+    {
+        todayEl.text(`Today is: ${dayText}, ${currentDate}nd of ${monthText}.`);
+    }
+    else //appending "th" to every other day
+    {
+        todayEl.text(`Today is: ${dayText}, ${currentDate}th of ${monthText}.`);
+    }
+    
+}
 //will get data from local storage. If none is found, return empty, else return with saved events. Repeated each hour
 function getData(findHour)
 {
@@ -69,3 +156,5 @@ function createCalendar()
     }
 }
 
+displayToday();
+createCalendar();
